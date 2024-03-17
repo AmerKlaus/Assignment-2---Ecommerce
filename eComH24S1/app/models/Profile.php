306f28af0fbec849e8f3393ee8fc1a61091eed22
olivier leone <fatecasual@gmail.com>
2024-03-17 19:11:26 -0400

    middle name fix

--- eComH24S1/app/models/Profile.php
+++ eComH24S1/app/models/Profile.php
@@ -7,17 +7,20 @@ class Profile extends \app\core\Model{
 	public $profile_id;//PK
 	public $user_id;
 	public $first_name;
+	public $middle_name;
 	public $last_name;
+
 
 	//CRUD
 
 	//create
 	public function insert(){
-		$SQL = 'INSERT INTO profile(user_id,first_name,last_name) VALUE (:user_id,:first_name,:last_name)';
+		$SQL = 'INSERT INTO profile(user_id,first_name,middle_name,last_name) VALUE (:user_id,:first_name,:middle_name,:last_name,)';
 		$STMT = self::$_conn->prepare($SQL);
 		$STMT->execute(
 			['user_id'=>$this->user_id,
 			'first_name'=>$this->first_name,
+			'middle_name'=>$this->middle_name,
 			'last_name'=>$this->last_name]
 		);
 	}
@@ -56,11 +59,12 @@ class Profile extends \app\core\Model{
 	//update
 	//you can't change the user_id that's a business logic choice that gets implemented in the model
 	public function update(){
-		$SQL = 'UPDATE profile SET first_name=:first_name,last_name=:last_name WHERE profile_id = :profile_id';
+		$SQL = 'UPDATE profile SET first_name=:first_name,middle_name=:middle_name,last_name=:last_name WHERE profile_id = :profile_id';
 		$STMT = self::$_conn->prepare($SQL);
 		$STMT->execute(
 			['profile_id'=>$this->profile_id,
 			'first_name'=>$this->first_name,
+			'middle_name'=>$this->middle_name,
 			'last_name'=>$this->last_name]
 		);
 	}
--- eComH24S1/app/views/Profile/create.php
+++ eComH24S1/app/views/Profile/create.php
@@ -10,12 +10,22 @@
 			<div class="form-group">
 				<label>First name:<input type="text" class="form-control" name="first_name" placeholder="Jon" /></label>
 			</div>
+
+
+			<div class="form-group">
+				<label>Middle name:<input type="text" class="form-control" name="middle_name" placeholder="me" /></label>
+			</div>
+
+
+
 			<div class="form-group">
 				<label>Last name:<input type="text" class="form-control" name="last_name" placeholder="Doe" /></label>
 			</div>
+
+
 			<div class="form-group">
 				<input type="submit" name="action" value="Record my profile" /> 
-				<a href='/Profile/index'>Cancel</a> (should not be here)
+				
 			</div>
 		</form>
 	</div>
