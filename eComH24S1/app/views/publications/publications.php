df027e685f9786da65906e8a96a36bc6c528cc46
olivier leone <fatecasual@gmail.com>
2024-03-20 20:12:16 -0400

    Search function done

--- eComH24S1/app/views/publications/publications.php
+++ eComH24S1/app/views/publications/publications.php
@@ -13,42 +13,47 @@
 <body>
     <h1>All Publications</h1>
 
-   
-<?php foreach ($publications as $publication): ?>
-    <div>
-        <?php if (isset($publication['publication_title'])): ?>
-            <h2><a href="/Publications/content/<?php echo $publication['publication_id']; ?>">
-                <?php echo $publication['publication_title']; ?>
-            </a></h2>
-            <?php if (isset($publication['publication_text'])): ?>
-                <p><?php echo $publication['publication_text']; ?></p>
-            <?php endif; ?>
+    <!-- Search Form -->
+    <form action="/Publications/index" method="GET">
+        <input type="text" name="q" placeholder="Search publications">
+        <button type="submit">Search</button>
+    </form>
 
-            <!-- Add comment form -->
-            <form action="/Publications/addComment/<?php echo $publication['publication_id']; ?>" method="post">
-                <label for="comment_text">Add a Comment:</label><br>
-                <textarea id="comment_text" name="comment_text"></textarea><br>
-                <input type="submit" value="Submit">
-            </form>
-
-            <!-- Display comments associated with this publication -->
-            <?php if (isset($comment)): // Check for $comment instead of $commentModel ?>
-                <?php $comments = $comment->getCommentsByPublicationId($publication['publication_id']); ?>
-                <?php if (!empty($comments)): ?>
-                    <ul>
-                        <?php foreach ($comments as $comment): ?>
-                            <li><a href='/Comment/view/<?php echo $comment->publication_comment_id; ?>'>View Comment</a></li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php else: ?>
-                    <p>No comments yet.</p>
+    <!-- Display Search Results -->
+    <?php foreach ($publications as $publication): ?>
+        <div>
+            <?php if (isset($publication['publication_title'])): ?>
+                <h2><a href="/Publications/content/<?php echo $publication['publication_id']; ?>">
+                    <?php echo $publication['publication_title']; ?>
+                </a></h2>
+                <?php if (isset($publication['publication_text'])): ?>
+                    <p><?php echo $publication['publication_text']; ?></p>
                 <?php endif; ?>
+
+                <!-- Add comment form -->
+                <form action="/Publications/addComment/<?php echo $publication['publication_id']; ?>" method="post">
+                    <label for="comment_text">Add a Comment:</label><br>
+                    <textarea id="comment_text" name="comment_text"></textarea><br>
+                    <input type="submit" value="Submit">
+                </form>
+
+                <!-- Display comments associated with this publication -->
+                <?php if (isset($comment)): // Check for $comment instead of $commentModel ?>
+                    <?php $comments = $comment->getCommentsByPublicationId($publication['publication_id']); ?>
+                    <?php if (!empty($comments)): ?>
+                        <ul>
+                            <?php foreach ($comments as $comment): ?>
+                                <li><a href='/Comment/view/<?php echo $comment->publication_comment_id; ?>'>View Comment</a></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php else: ?>
+                        <p>No comments yet.</p>
+                    <?php endif; ?>
+                <?php endif; ?>
+
             <?php endif; ?>
-
-        <?php endif; ?>
-    </div>
-<?php endforeach; ?>
-
+        </div>
+    <?php endforeach; ?>
 
     <a href="/Publications/create">POST</a>
 </body>
