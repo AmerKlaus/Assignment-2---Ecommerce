a9c014539d8b7338386b86a84e3cf303d3d70296
Amer <amer1jawabra@gmail.com>
2024-03-20 20:31:52 -0400

    nothing

--- eComH24S1/app/views/publications/publications.php
+++ eComH24S1/app/views/publications/publications.php
@@ -30,27 +30,6 @@
                     <p><?php echo $publication['publication_text']; ?></p>
                 <?php endif; ?>
 
-                <!-- Add comment form -->
-                <form action="/Publications/addComment/<?php echo $publication['publication_id']; ?>" method="post">
-                    <label for="comment_text">Add a Comment:</label><br>
-                    <textarea id="comment_text" name="comment_text"></textarea><br>
-                    <input type="submit" value="Submit">
-                </form>
-
-                <!-- Display comments associated with this publication -->
-                <?php if (isset($comment)): // Check for $comment instead of $commentModel ?>
-                    <?php $comments = $comment->getCommentsByPublicationId($publication['publication_id']); ?>
-                    <?php if (!empty($comments)): ?>
-                        <ul>
-                            <?php foreach ($comments as $comment): ?>
-                                <li><a href='/Comment/view/<?php echo $comment->publication_comment_id; ?>'>View Comment</a></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php else: ?>
-                        <p>No comments yet.</p>
-                    <?php endif; ?>
-                <?php endif; ?>
-
             <?php endif; ?>
         </div>
     <?php endforeach; ?>
