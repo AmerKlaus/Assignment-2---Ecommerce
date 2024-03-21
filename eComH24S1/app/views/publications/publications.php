289915f0127fc6547a4cff9ee46833a367d4ccdc
olivier leone <fatecasual@gmail.com>
2024-03-20 20:31:05 -0400

    css stlying partially done

--- eComH24S1/app/views/publications/publications.php
+++ eComH24S1/app/views/publications/publications.php
@@ -1,61 +1,64 @@
-<!-- Inside publications.php -->
+<!DOCTYPE html>
 <html>
 
 <head>
     <title>All Publications</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="../css/styles.css" rel="stylesheet" type="text/css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <h1>All Publications</h1>
+    <div class="container">
+        <h1>All Publications</h1>
 
-    <!-- Search Form -->
-    <form action="/Publications/index" method="GET">
-        <input type="text" name="q" placeholder="Search publications">
-        <button type="submit">Search</button>
-    </form>
+        <!-- Search Form -->
+        <form action="/Publications/index" method="GET">
+            <input type="text" name="q" placeholder="Search publications">
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
 
-    <!-- Display Search Results -->
-    <?php foreach ($publications as $publication): ?>
+        <!-- Display Search Results -->
+        <?php foreach ($publications as $publication): ?>
         <div>
             <?php if (isset($publication['publication_title'])): ?>
-                <h2><a href="/Publications/content/<?php echo $publication['publication_id']; ?>">
+            <h2><a href="/Publications/content/<?php echo $publication['publication_id']; ?>">
                     <?php echo $publication['publication_title']; ?>
                 </a></h2>
-                <?php if (isset($publication['publication_text'])): ?>
-                    <p><?php echo $publication['publication_text']; ?></p>
-                <?php endif; ?>
-
-                <!-- Add comment form -->
-                <form action="/Publications/addComment/<?php echo $publication['publication_id']; ?>" method="post">
-                    <label for="comment_text">Add a Comment:</label><br>
-                    <textarea id="comment_text" name="comment_text"></textarea><br>
-                    <input type="submit" value="Submit">
-                </form>
-
-                <!-- Display comments associated with this publication -->
-                <?php if (isset($comment)): // Check for $comment instead of $commentModel ?>
-                    <?php $comments = $comment->getCommentsByPublicationId($publication['publication_id']); ?>
-                    <?php if (!empty($comments)): ?>
-                        <ul>
-                            <?php foreach ($comments as $comment): ?>
-                                <li><a href='/Comment/view/<?php echo $comment->publication_comment_id; ?>'>View Comment</a></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php else: ?>
-                        <p>No comments yet.</p>
-                    <?php endif; ?>
-                <?php endif; ?>
-
             <?php endif; ?>
-        </div>
-    <?php endforeach; ?>
+            <?php if (isset($publication['publication_text'])): ?>
+            <p><?php echo $publication['publication_text']; ?></p>
+            <?php endif; ?>
 
-    <a href="/Publications/create">POST</a>
+            <!-- Add comment form -->
+            <form action="/Publications/addComment/<?php echo $publication['publication_id']; ?>" method="post">
+                <label for="comment_text">Add a Comment:</label><br>
+                <textarea id="comment_text" name="comment_text"></textarea><br>
+                <input type="submit" value="Submit" class="btn btn-primary">
+            </form>
+
+            <!-- Display comments associated with this publication -->
+            <?php if (isset($comment)): // Check for $comment instead of $commentModel ?>
+            <?php $comments = $comment->getCommentsByPublicationId($publication['publication_id']); ?>
+            <?php if (!empty($comments)): ?>
+            <ul>
+                <?php foreach ($comments as $comment): ?>
+                <li><a href='/Comment/view/<?php echo $comment->publication_comment_id; ?>'>View Comment</a></li>
+                <?php endforeach; ?>
+            </ul>
+            <?php else: ?>
+            <p>No comments yet.</p>
+            <?php endif; ?>
+            <?php endif; ?>
+
+        </div>
+        <?php endforeach; ?>
+
+        <a href="/Publications/create" class="btn btn-primary">POST</a>
+    </div>
 </body>
 
 </html>
